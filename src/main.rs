27d8d577a47b7cb52a205243b572f1e33194ecd1
Zach Legein <zach.legein@hilton.com>
2022-04-20 11:22:14 -0500

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::Serialize;
+use rocket::serde::json::Json;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +9,13 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/rates", routes![rates])
+}
+
+#[derive(Serialize)]
+struct Rates { foo: String }
+
+#[get("/rates", format="json")]
+fn rates() -> Json<Rates> {
+    Json(Rates { foo: "bar".to_string() })
 }
