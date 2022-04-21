98d77ac17e604f7eeba9509880a3195598eab41e
Zach Legein <zach.legein@hilton.com>
2022-04-21 15:16:07 -0500

    final

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use rocket::serde::json::{ json, Value, Json };
 use rocket::serde::{ Deserialize, Serialize };
 use rusty_money::iso::Currency;
 use rusty_money::{ExchangeRate, iso, Money, MoneyError};
-use rust_decimal_macros::*;
 use rust_decimal::Decimal;
 use std::collections::HashMap;
 
@@ -51,35 +50,34 @@ struct Input<'r> {
 #[serde(rename_all = "camelCase")]
 pub struct ConvertResponse {
     code: String,
-    amount: f32
+    amount: Decimal,
+    formatted: String
 }
 
 #[get("/rate/<code>")]
-fn rate(code: String) -> Result<Json<Rate>, NotFound<String>> {
-    let rate = get_rate(code);
-    if let Some(result) = rate { 
-        return Ok(Json(Rate{ code: code, factor: result }));
+fn rate(code: &str) -> Result<Json<Rate>, NotFound<String>> {
+    let rate = get_rate(code.to_string());
+    if let Some(result) = rate {
+        return Ok(Json(Rate{ code: code.to_string(), factor: result }));
     }
     Err(NotFound("Could not find requested currency code.".to_string()))
 }
 
 #[post("/convert", data="<input>")]
-fn convert(input: Json<Input<'_>>) -> Json<ConvertResponse> {
+fn convert(input: Json<Input<'_>>) -> Result<Json<ConvertResponse>, NotFound<String>> {
     
-    let amount = Money::from_decimal(input.amount, iso::USD);
-    exchange(amount, )
-    let money = rate.convert(amount); 
+    let amount = Money::from_decimal(Decimal::from_f32_retain(input.amount).unwrap(), iso::USD);
+    let money = exchange(amount, input.code.to_string());
     match money {
-        Ok(result) => return Json(ConvertResponse{code: rate_code.to_string(), amount: result.to_string()  }),
-        Err(err) => panic!("Could not convert {}", err)
+        Ok(result) => return Ok(Json(ConvertResponse{code: input.code.to_string(), amount: *result.amount(), formatted: format!("{}", result) })),
+        Err(err) => Err(NotFound(format!("Currency not found - :{}", err)))
     }
 }
 
 fn exchange(amount: Money<Currency>, code: String) -> Result<Money<Currency>, MoneyError> {
     let rate = get_rate(code.to_string()).expect("could not get rate");
     let rate_code = iso::find(&code[..]).expect("could not find currency");
-    let rate = ExchangeRate::new(iso::USD, rate_code, Decimal::from_f32_retain(rate).unwrap());
-    
+    let rate = ExchangeRate::new(iso::USD, rate_code, Decimal::from_f32_retain(rate).unwrap()).unwrap();
     rate.convert(amount)
 }
 
